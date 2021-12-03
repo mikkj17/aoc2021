6259b61196f31a9a3c1d585f3ccfc1c0fa139ffa
Mikkel Møller Jensen <32734906+mikkj17@users.noreply.github.com>
2021-12-03 08:01:55 +0100

    add nice solution to day 3

--- solutions/03.py
+++ solutions/03.py
@@ -16,73 +16,34 @@ test = """\
 01010"""
 
 
+def most_freq(lines: List[str], idx: int) -> str:
+    freqs = {'1': 0, '0': 0}
+    for line in lines:
+        freqs[line[idx]] += 1
+    return max(freqs.items(), key=lambda f: f[1])[0]
+
+
 def _1(inp: str) -> int:
-    g = ''
-    e = ''
+    gamma = ''
+    epsilon = ''
     lines = inp.splitlines()
-    size = len(lines[0])
-    for i in range(size):
-        zeros = 0
-        ones = 0
-        for line in lines:
-            bit = line[i]
-            if bit == '0':
-                zeros += 1
-            else:
-                ones += 1
-        if zeros >= ones:
-            g += '0'
-            e += '1'
-        else:
-            g += '1'
-            e += '0'
-    return int(g, 2) * int(e, 2)
-
-def ox(considered: List[str], idx: int) -> str:
-    zeros = 0
-    ones = 0
-    for line in considered:
-        bit = line[idx]
-        if bit == '0':
-            zeros += 1
-        else:
-            ones += 1
-    if ones >= zeros:
-        mst = '1'
-    else:
-        mst = '0'
-
-    considered = [line for line in considered if line[idx] == mst]
-    if len(considered) == 1:
-        return considered[0]
-    else:
-        return ox(considered, idx+1)
-
-def co(considered: List[str], idx: int) -> str:
-    zeros = 0
-    ones = 0
-    for line in considered:
-        bit = line[idx]
-        if bit == '0':
-            zeros += 1
-        else:
-            ones += 1
-    if ones >= zeros:
-        lst = '0'
-    else:
-        lst = '1'
-
-    considered = [line for line in considered if line[idx] == lst]
-    if len(considered) == 1:
-        return considered[0]
-    else:
-        return co(considered, idx+1)
+    for i in range(len(lines[0])):
+        mst_freq = most_freq(lines, i)
+        gamma += mst_freq
+        epsilon += str(1 - int(mst_freq))
+    return int(gamma, 2) * int(epsilon, 2)
 
 
 def _2(inp: str) -> int:
-    oxy = ox(inp.splitlines(), 0)  
-    co2 = co(inp.splitlines(), 0)
-    return int(oxy, 2) * int(co2, 2)
+    lines = inp.splitlines()
+    return int(func(lines, 0, True), 2) * int(func(lines, 0, False), 2)
+
+
+def func(considered: List[str], idx: int, most_common: bool) -> str:
+    mst_freq = most_freq(considered, idx)
+    good_bit = mst_freq if most_common else str(1 - int(mst_freq))
+    remaining = [line for line in considered if line[idx] == good_bit]
+    return remaining[0] if len(remaining) == 1 else func(remaining, idx+1, most_common)
 
 
 if __name__ == '__main__':
